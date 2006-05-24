d21f9b774b90842cde852b33e33798f72a78f1d2
andrewb <andrewb@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2006-05-24 21:48:47 +0000

    in an attempt to follow press release best practices, adding in several key press release fields as per guidelines at: http://www.canadaone.com/promote/newsrelease4.html

--- content/PressRelease.py
+++ content/PressRelease.py
@@ -31,9 +31,25 @@ from Products.PressRoom.config import *
 
 # Schema declaration
 schema = ATNewsItemSchema.copy()
+schema['title'].widget.label = 'Headline'
+schema['title'].widget.i18n_domain = 'pressroom'
 
 schema = schema + Schema((
 
+    StringField('releaseTiming',
+                required=0,
+                searchable=0,
+                primary=False,
+                default="FOR IMMEDIATE RELEASE",
+                languageIndependent=0,
+                index="FieldIndex:brains",
+                widget=StringWidget(
+                    label='Release timing',
+                    label_msgid = "label_release_timing",
+                    description="""When should this release be distributed (e.g. FOR IMMEDIATE RELEASE and HOLD FOR RELEASE UNTIL MM/DD/YYYY)""",
+                    description_msgid = "help_release_timing",
+                    i18n_domain = "pressroom",),
+                ),
     StringField('subhead',
                 required=0,
                 primary=False,
@@ -56,8 +72,21 @@ schema = schema + Schema((
                 widget=StringWidget(
                     label='Press release location',
                     label_msgid = "label_location",
-                    description='Typically press releases have an associated location in a common format (i.e. <City>, <State>)',
+                    description='Typically press releases have an associated location in a common format (i.e. City, State)',
                     description_msgid = "help_location",
+                    i18n_domain = "pressroom",),
+                ),
+    DateTimeField('releaseDate',
+                required=0,
+                searchable=0,
+                primary=False,
+                languageIndependent=0,
+                index="FieldIndex:brains",
+                widget=CalendarWidget(
+                    label='Press release date',
+                    label_msgid = "label_release_date",
+                    description='Provide a date for when this press relase will be distributed',
+                    description_msgid = "help_release_date",
                     i18n_domain = "pressroom",),
                 ),
     ReferenceField('releaseContacts',
@@ -75,16 +104,19 @@ schema = schema + Schema((
                         force_close_on_insert = True,
                         label = "Release Contact(s)",
                         label_msgid = "label_release_contacts",
-                        description = "",
+                        description = "Contact with assigned release contacts will be encouraged for further information",
                         description_msgid = "help_release_contacts",
-                        i18n_domain = "plone",
+                        i18n_domain = "pressroom",
                         visible = {'edit' : 'visible', 'view' : 'visible' }
                 )
             )
     ),)
 
+schema.moveField('releaseTiming', before='title')
 schema.moveField('subhead', after='title')
 schema.moveField('location', after='subhead')
+schema.moveField('releaseDate', after='location')
+
 finalizeATCTSchema(schema)
 
 class PressRelease(ATNewsItem):
